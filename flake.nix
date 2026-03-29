a510d6183db0c0ebe179a154e78631832972f8de
Quint Daenen <quint@daenen.email>
2026-03-29 13:53:40 +0200

    feat: add quill and didc to nix dev shell

--- flake.nix
+++ flake.nix
@@ -53,6 +53,48 @@
             chmod +x $out/bin/pocket-ic
           '';
         };
+        didc = pkgs.stdenv.mkDerivation rec {
+          name = "didc";
+          version = "2025-12-18";
+          src = pkgs.fetchurl {
+            url = let
+              suffix = if pkgs.stdenv.isDarwin then "macos" else "linux64";
+            in "https://github.com/dfinity/candid/releases/download/${version}/didc-${suffix}";
+            sha256 = if pkgs.stdenv.isDarwin
+              then "sha256-58TyRQ6n63U1qf6IUxAvYGiyeH1hl/amWH3ZbH9RELs="
+              else "sha256-Mmk8dtnG/g8nPywev35Iujw4PpJeWv0X3SZKBq7Z+8w=";
+          };
+          dontUnpack = true;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/bin/didc
+            chmod +x $out/bin/didc
+          '';
+        };
+        quill = pkgs.stdenv.mkDerivation rec {
+          name = "quill";
+          version = "0.5.4";
+          src = pkgs.fetchurl {
+            url = let
+              suffix = {
+                "x86_64-darwin" = "macos-x86_64";
+                "aarch64-darwin" = "macos-arm64";
+                "x86_64-linux" = "linux-x86_64";
+              }.${system};
+            in "https://github.com/dfinity/quill/releases/download/v${version}/quill-${suffix}";
+            sha256 = {
+              "x86_64-darwin" = "sha256-zWnzLiyyS6vjREketCzAwA/CmHDf224mYequz23YscM=";
+              "aarch64-darwin" = "sha256-cMxivvl+gT64KOgqcSmQNlNtP+xXwUKK28o/g9NjI1s=";
+              "x86_64-linux" = "sha256-cxBVeDcghuNJpF6d/WfP4F8AX/gCREnTWWmqqc/0o1c=";
+            }.${system};
+          };
+          dontUnpack = true;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/bin/quill
+            chmod +x $out/bin/quill
+          '';
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -62,8 +104,10 @@
             git
             gcc
             wabt
+            didc
             ic-wasm
             pocket-ic
+            quill
             rustToolchain
           ];
           TZ = "UTC";
